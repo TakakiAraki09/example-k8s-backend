d58b29bba669327399ba5ee0b83752e23a6b2102
ArakiTakaki09 <arakitakaki.work+main@gmail.com>
2025-03-04 21:03:22 +0900

    update

--- schema/db/schema.sql
+++
@@ -1,6 +0,0 @@
--- Create "example_table" table
-CREATE TABLE `example_table` (`id` int unsigned NOT NULL AUTO_INCREMENT, `title` varchar(100) NULL, `hoge` varchar(100) NULL, `fuga` varchar(100) NULL, PRIMARY KEY (`id`)) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
--- Create "users" table
-CREATE TABLE `users` (`id` int unsigned NOT NULL AUTO_INCREMENT, `name` varchar(100) NULL, PRIMARY KEY (`id`)) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
--- Create "todos" table
-CREATE TABLE `todos` (`id` int unsigned NOT NULL AUTO_INCREMENT, `text` varchar(100) NULL, `done` varchar(100) NULL, `user_id` int unsigned NOT NULL, PRIMARY KEY (`id`), INDEX `user_id` (`user_id`), CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
