21be3bd2bb88341094885e3e91bf47a7c986120d
ArakiTakaki09 <arakitakaki.work+main@gmail.com>
2025-04-01 21:48:49 +0900

    envがなくても一応動くようにしておく

--- server.go
+++ server.go
@@ -31,7 +31,7 @@ import (
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	port := os.Getenv(constants.EnvPort)
